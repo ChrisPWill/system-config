0cc070ca710a17e913cc3be5b05f0b3104038301
Chris Williams <chris@chrispwill.com>
2024-06-19 01:59:41 +1000

    Comments and fixes

--- users/me/home-manager/nixvim/plugins/default.nix
+++ users/me/home-manager/nixvim/plugins/default.nix
@@ -1,8 +1,17 @@
-{...}: {
+{config, ...}: let
+  cfg = config.programs.nixvim.custom;
+in {
   imports = [
+    # Contains cmp and so on
     ./completions.nix
+
+    # Language server config
     ./lsp.nix
+
+    # Useful popup window tool
     ./telescope.nix
+
+    # Highlighting etc.
     ./treesitter.nix
   ];
 
@@ -30,16 +39,35 @@
   ];
 
   programs.nixvim.plugins = {
+    # Bottom bar with statuses etc.
     airline = {
       enable = true;
       settings.powerline_fonts = true;
     };
+
+    # Top bar with tabs etc.
+    bufferline.enable = true;
+
+    # Git tools
     fugitive.enable = true;
-    fzf-lua.enable = true;
+
+    # Conflict resolution plugin (co for choose ours, ct for choose theirs,
+    # cb for both, c0 for none, ]x for next conflict, [x for prev conflict)
     git-conflict.enable = true;
+
+    # Shows git changes on left side
     gitgutter.enable = true;
+
+    # Highlights same references
+    illuminate.enable = true;
+
+    # Leap to parts of file with x and X
     leap.enable = true;
+
+    # Notification manager
     notify.enable = true;
+
+    # File tree on side
     nvim-tree = {
       enable = true;
       disableNetrw = true;
@@ -51,8 +79,16 @@
         preserveWindowProportions = true;
       };
     };
+
+    # Add closing brackets/tags/etc.
     surround.enable = true;
+
+    # Make background transparent
     transparent.enable = true;
+
+    # Diagnostics/symbols/etc. at bottom \xx
     trouble.enable = true;
+
+    copilot-lua.enable = cfg.enableCopilot;
   };
 }
