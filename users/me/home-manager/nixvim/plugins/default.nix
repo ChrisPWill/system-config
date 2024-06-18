d9e9cd340505bb3aee2342b77e91504508430fce
Chris Williams <chris@chrispwill.com>
2024-06-19 01:46:20 +1000

    Extra plugins

--- users/me/home-manager/nixvim/plugins/default.nix
+++ users/me/home-manager/nixvim/plugins/default.nix
@@ -6,8 +6,53 @@
     ./treesitter.nix
   ];
 
+  programs.nixvim.keymaps = [
+    {
+      key = "<leader>ntt";
+      action = "<cmd>NvimTreeFindFile<cr>NvimTreeFocus<cr>";
+    }
+    {
+      key = "<leader>ntf";
+      action = "<cmd>NvimTreeFindFile<cr>";
+    }
+    {
+      key = "<leader>ntr";
+      action = "<cmd>NvimTreeRefresh<cr>";
+    }
+    {
+      key = "<leader>ntx";
+      action = "<cmd>NvimTreeClose<cr>";
+    }
+    {
+      key = "<leader>nt[";
+      action = "<cmd>NvimTreeCollapse<cr>";
+    }
+  ];
+
   programs.nixvim.plugins = {
-    git-conflict.enable = true;
+    airline = {
+      enable = true;
+      settings.powerline_fonts = true;
+    };
+    fugitive.enable = true;
     fzf-lua.enable = true;
+    git-conflict.enable = true;
+    gitgutter.enable = true;
+    leap.enable = true;
+    notify.enable = true;
+    nvim-tree = {
+      enable = true;
+      disableNetrw = true;
+      hijackCursor = true;
+      modified.enable = true;
+      updateFocusedFile.enable = true;
+      renderer.groupEmpty = true;
+      view = {
+        preserveWindowProportions = true;
+      };
+    };
+    surround.enable = true;
+    transparent.enable = true;
+    trouble.enable = true;
   };
 }
