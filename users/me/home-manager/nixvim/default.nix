b8ec2be0874d4cee9f6b545da46670978d07ae72
Chris Williams <chris@chrispwill.com>
2024-06-19 00:08:23 +1000

    Add cmp

--- users/me/home-manager/nixvim/default.nix
+++ users/me/home-manager/nixvim/default.nix
@@ -1,9 +1,16 @@
-{...}: {
+{lib, ...}: {
   imports = [
     ./plugins/treesitter.nix
+    ./plugins/completions.nix
   ];
 
-  programs.nixvim = {
+  options = {
+    programs.nixvim.custom = {
+      enableCopilot = lib.mkEnableOption "Enable Copilot in Neovim";
+    };
+  };
+
+  config.programs.nixvim = {
     enable = true;
     defaultEditor = true;
   };
---
+++ users/me/home-manager/nixvim/plugins/completions.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.programs.nixvim.custom;
+in {
+  programs.nixvim.plugins = {
+    luasnip.enable = true;
+    cmp-nvim-lsp.enable = true;
+    cmp = {
+      enable = true;
+      settings = {
+        sources =
+          [
+            {name = "nvim_lsp";}
+          ]
+          ++ lib.optionals cfg.enableCopilot [{name = "copilot";}];
+        mapping = {
+          "<CR>" = ''
+            cmp.mapping.confirm({
+              behavior = cmp.ConfirmBehavior.Replace,
+              select = false,
+              })
+          '';
+          "<C-space>" = "cmp.mapping.complete()";
+          "<Tab>" = ''
+            cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              elseif has_words_before() then
+                cmp.complete()
+              else
+                fallback()
+              end
+            end, {'i', 's'})
+          '';
+          "<S-Tab>" = ''
+            cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, {'i', 's'})
+          '';
+        };
+      };
+    };
+  };
+}
