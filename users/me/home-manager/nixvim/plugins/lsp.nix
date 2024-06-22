86d84979e364c653f2e474f9402a10a03659618a
Chris Williams <chris@chrispwill.com>
2024-06-22 13:49:42 +1000

    Add format on save with disable

--- users/me/home-manager/nixvim/plugins/lsp.nix
+++ users/me/home-manager/nixvim/plugins/lsp.nix
@@ -1,10 +1,30 @@
-{ utils }: { pkgs, ... }:
-let
+{utils}: {pkgs, ...}: let
   keymap = utils.keymap;
   keymapRaw = utils.keymapRaw;
-in
-{
+in {
   programs.nixvim = {
+    extraPackages = with pkgs;
+      [
+        # language servers
+        bash-language-server
+        dockerfile-language-server-nodejs
+        lua-language-server
+        nil
+        nodePackages.graphql-language-service-cli
+        nodePackages.typescript-language-server
+        rust-analyzer
+        tailwindcss-language-server
+        vscode-langservers-extracted
+
+        # formatters
+        prettierd
+        stylua
+        alejandra
+      ]
+      ++ lib.optionals pkg.stdenv.isLinux [
+        wl-clipboard-rs
+      ];
+
     plugins.lsp = {
       enable = true;
       servers = {
@@ -15,73 +35,89 @@ in
         jsonls.enable = true;
         lua-ls.enable = true;
         nil-ls.enable = true;
-        nil-ls.settings.formatting.command = [ "alejandra" ];
+        nil-ls.settings.formatting.command = ["alejandra"];
         rust-analyzer.enable = true;
         tailwindcss.enable = true;
         tsserver.enable = true;
       };
     };
 
+    # Formatter
     plugins.conform-nvim = {
       enable = true;
+      formatOnSave = ''
+        function(bufnr)
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_format = "fallback" }
+        end
+      '';
       formattersByFt = {
         # prettier
-        javascript = [ [ "prettierd prettier" ] ];
-        javascriptreact = [ [ "prettierd prettier" ] ];
-        typescript = [ [ "prettierd prettier" ] ];
-        typescriptreact = [ [ "prettierd prettier" ] ];
-        css = [ [ "prettierd prettier" ] ];
-        html = [ [ "prettierd prettier" ] ];
-        json = [ [ "prettierd prettier" ] ];
-        yaml = [ [ "prettierd prettier" ] ];
-        markdown = [ [ "prettierd prettier" ] ];
-        graphql = [ [ "prettierd prettier" ] ];
-        "markdown.mdx" = [ [ "prettierd prettier" ] ];
+        javascript = [["prettierd prettier"]];
+        javascriptreact = [["prettierd prettier"]];
+        typescript = [["prettierd prettier"]];
+        typescriptreact = [["prettierd prettier"]];
+        css = [["prettierd prettier"]];
+        html = [["prettierd prettier"]];
+        json = [["prettierd prettier"]];
+        yaml = [["prettierd prettier"]];
+        markdown = [["prettierd prettier"]];
+        graphql = [["prettierd prettier"]];
+        "markdown.mdx" = [["prettierd prettier"]];
 
-        rust = [ "rustfmt" ];
+        rust = ["rustfmt"];
 
-        nix = [ "alejandra" ];
+        nix = ["alejandra"];
 
-        lua = [ "stylua" ];
+        lua = ["stylua"];
       };
     };
 
-    extraPackages = with pkgs; [
-      # language servers
-      bash-language-server
-      dockerfile-language-server-nodejs
-      lua-language-server
-      nil
-      nodePackages.graphql-language-service-cli
-      nodePackages.typescript-language-server
-      rust-analyzer
-      tailwindcss-language-server
-      vscode-langservers-extracted
-
-      # formatters
-      prettierd
-      stylua
-      alejandra
-    ] ++ lib.optionals pkg.stdenv.isLinux [
-      wl-clipboard-rs
-    ];
-
+    userCommands = {
+      FormatEnable = {
+        bang = true;
+        command.__raw = ''
+          function(args)
+            if args.bang then
+              vim.b.disable_autoformat = false
+            else
+              vim.g.disable_autoformat = false
+            end
+          end
+        '';
+      };
+      FormatDisable = {
+        bang = true;
+        command.__raw = ''
+          function(args)
+            if args.bang then
+              vim.b.disable_autoformat = true
+            else
+              vim.g.disable_autoformat = true
+            end
+          end
+        '';
+      };
+    };
 
     keymaps = [
-      (keymap "<A-]>" "<C-I>" "Go to newer jump" { })
-      (keymap "<A-[>" "<C-O>" "Go to older jump" { })
+      (keymap "<A-]>" "<C-I>" "Go to newer jump" {})
+      (keymap "<A-[>" "<C-O>" "Go to older jump" {})
 
       # LSP specific
-      (keymapRaw "KK" "vim.lsp.buf.hover" "Show LSP info" { })
-      (keymapRaw "KA" "vim.lsp.buf.code_action" "LSP Code Action" { })
-      (keymapRaw "KE" "vim.lsp.buf.rename" "LSP Rename" { })
-      (keymapRaw "KD" "vim.lsp.buf.definition" "LSP Open Definition" { })
-      (keymapRaw "KI" "vim.lsp.buf.implementation" "LSP Open Implementations" { })
-      (keymapRaw "KN" "vim.diagnostic.goto_next" "LSP Goto next diagnostic" { })
-      (keymapRaw "KP" "vim.diagnostic.goto_prev" "LSP Goto prev diagnostic" { })
-      (keymapRaw "KR" "require('telescope.builtin').lsp_references" "Show LSP References" { })
-      (keymap "KT" "<cmd>Telescope diagnostics<cr>" "Show LSP Diagnostics" { })
-      (keymapRaw "KF" "function() require('conform').format({ async = true, timeout_ms = 10000 }) end" "LSP Format" { })
+      (keymapRaw "KK" "vim.lsp.buf.hover" "Show LSP info" {})
+      (keymapRaw "KA" "vim.lsp.buf.code_action" "LSP Code Action" {})
+      (keymapRaw "KE" "vim.lsp.buf.rename" "LSP Rename" {})
+      (keymapRaw "KD" "vim.lsp.buf.definition" "LSP Open Definition" {})
+      (keymapRaw "KI" "vim.lsp.buf.implementation" "LSP Open Implementations" {})
+      (keymapRaw "KN" "vim.diagnostic.goto_next" "LSP Goto next diagnostic" {})
+      (keymapRaw "KP" "vim.diagnostic.goto_prev" "LSP Goto prev diagnostic" {})
+      (keymapRaw "KR" "require('telescope.builtin').lsp_references" "Show LSP References" {})
+      (keymap "KT" "<cmd>Telescope diagnostics<cr>" "Show LSP Diagnostics" {})
+      (keymapRaw "KF" "function() require('conform').format({ async = true, timeout_ms = 10000 }) end" "LSP Format" {})
     ];
   };
 }
